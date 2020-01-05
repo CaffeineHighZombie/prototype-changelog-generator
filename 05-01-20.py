da86039564eb2b2d0489b0873629e229df0d3abc
RS Saranyaraj <rs.saranyaraj@gmail.com>
2020-01-05 16:32:23 -0600

    feat: Add code chunks to process output as per usage of the type of the commit and calling appropriate function for it
    Changes to be committed:
    	modified:   05-01-20.py

--- 05-01-20.py
+++ 05-01-20.py
@@ -39,3 +39,83 @@ for line in f:
     print(commit_hash)
     print(commit_message)
     
+def feat_commit(commit_msg, commit_hash):
+    print(commit_msg, commit_hash)
+    
+def fix_commit(commit_msg, commit_hash):
+    print(commit_msg, commit_hash)
+    
+def chore_commit(commit_msg, commit_hash):
+    print(commit_msg, commit_hash)
+    
+def chore_commit(commit_msg, commit_hash):
+    print("chore_commit:", commit_msg, commit_hash)
+    
+def fix_commit(commit_msg, commit_hash):
+    print("fix_commit:", commit_msg, commit_hash)
+    
+def feat_commit(commit_msg, commit_hash):
+    print("feat_commit: ", commit_msg, commit_hash)
+    
+def else_commit(commit_msg, commit_hash):
+    print("ELSE_commit: ", commit_msg, commit_hash)
+    
+for line in f:
+    commit_hash, commit_message = line.rstrip('\n\r').split('--HASH--')
+    if commit_message.lower().startswith('feat:'):
+        feat_commit(commit_message, commit_hash)
+    elif commit_message.lower().startswith('fix:'):
+        fix_commit(commit_message, commit_hash)
+    elif commit_message.lower().startswith('chore:'):
+        chore_commit(commit_message, commit_hash)
+    else:
+        else_commit(commit_message, commit_hash)
+        
+f = open('sample-commits-10')
+for line in f:
+    commit_hash, commit_message = line.rstrip('\n\r').split('--HASH--')
+    if commit_message.lower().startswith('feat:'):
+        feat_commit(commit_message, commit_hash)
+    elif commit_message.lower().startswith('fix:'):
+        fix_commit(commit_message, commit_hash)
+    elif commit_message.lower().startswith('chore:'):
+        chore_commit(commit_message, commit_hash)
+    else:
+        else_commit(commit_message, commit_hash)
+        
+def else_commit(commit_msg, commit_hash):
+    print("##ELSE_commit##", commit_msg, commit_hash)
+    
+def feat_commit(commit_msg, commit_hash):
+    print("##feat_commit##", commit_msg, commit_hash)
+    
+def fix_commit(commit_msg, commit_hash):
+    print("##fix_commit##", commit_msg, commit_hash)
+    
+def chore_commit(commit_msg, commit_hash):
+    print("##chore_commit##", commit_msg, commit_hash)
+    
+f = open('sample-commits-10')
+for line in f:
+    commit_hash, commit_message = line.rstrip('\n\r').split('--HASH--')
+    if commit_message.lower().startswith('feat:'):
+        feat_commit(commit_message, commit_hash)
+    elif commit_message.lower().startswith('fix:'):
+        fix_commit(commit_message, commit_hash)
+    elif commit_message.lower().startswith('chore:'):
+        chore_commit(commit_message, commit_hash)
+    else:
+        else_commit(commit_message, commit_hash)
+        
+f = open('sample-commits-10')
+for line in f:
+    commit_hash, commit_message = line.rstrip('\n\r').split('--HASH--')
+    if commit_message.lower().startswith('feat:'):
+        feat_commit(commit_message, commit_hash)
+    elif commit_message.lower().startswith('fix:'):
+        fix_commit(commit_message, commit_hash)
+    elif commit_message.lower().startswith('chore:'):
+        chore_commit(commit_message, commit_hash)
+    else:
+        else_commit(commit_message, commit_hash)
+        
