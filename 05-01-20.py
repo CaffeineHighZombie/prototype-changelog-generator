8636e1fa5dae949d472156f22e39ecdc4e5807db
RS Saranyaraj <rs.saranyaraj@gmail.com>
2020-01-06 00:37:17 -0600

    feat: experimental code chunk from ipython experimentation saving for posterity
    Changes to be committed:
    	modified:   05-01-20.py

--- 05-01-20.py
+++ 05-01-20.py
@@ -119,3 +119,61 @@ for line in f:
     else:
         else_commit(commit_message, commit_hash)
         
+get_ipython().run_line_magic('logstart', '05-01-20.py append')
+import git
+repo = git.Repo()
+commits = list(repo.iter_commits("master", max_counts=5))
+dir(repo)
+dir(repo.iter_commits)
+dir(repo.iter_commits())
+dir(repo.active_branch)
+repo.active_branch
+repo.active_branch.name
+repo.active_branch.log
+repo.active_branch.log()
+dir(repo.active_branch)
+dir(repo.active_branch.log)
+dir(repo.active_branch.log())
+commits = list(repo.iter_commits())
+commits
+len(commits)
+commit = commits[0]
+commit
+commit.author
+commit.message
+dir(commit)
+commit.list_items()
+commit.list_items(repo)
+commit.summary
+type(commit.summary)
+type(commit.hexsha)
+commit.hexsha
+commit.summary
+c1 = list(repo.iter_commits())
+len(c)
+len(c1)
+c2 = list(repo.iter_commits('3138d17'))
+len(c2)
+c2 = list(repo.iter_commits('3138d17..HEAD'))
+len(c2)
+c2 = list(repo.iter_commits('3138d17..da86039'))
+len(c2)
+s1 = "fix: don't produce template diagnostics when scope is invalid (#34460)"
+s1
+l1 = s1.lsplit(':', max_count=1)
+l1 = s1.split(':', max_count=1)
+l1 = s1.split(':', maxsplit=1)
+l1
+s2 = "fix(ivy): don't produce template diagnostics when scope is invalid (#34460)"
+s2 = "fix(ivy): don't produce template diagnostics when scope is invalid so:54  (#34460)"
+l2 = s2.split(':', maxsplit=1)
+l2
+l3 = l2[0].replace('fix','')
+l3
+l4 = l3.strip('()')
+l4
+s2
+l3
+l4
+l2
+exit()
