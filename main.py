d32d1e259819a1addde8f1145b657bc927039b68
RS Saranyaraj <rs.saranyaraj@gmail.com>
2020-01-05 19:57:06 -0600

    fix: Fixed the bug with malformed CHANGELOG.md file generation
    Changes to be committed:
    	modified:   main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import sys
-import pathlib
 import os
 import datetime
  
@@ -42,52 +41,50 @@ def main(filename):
             exception_string = 'Commit Hash: {} | Subject: {}'.format(commit_hash, commit_message)
             exception_list.append(exception_string)
 
-        ## Checking to see if there are any syntax violating commits for the current changelog
-        ## if so, then writing it to a reference file with commit hash and subject for the 
-        ## relase manage to git rebase and squah them. Else, writing the changelog.
-        if len(exception_list) != 0:
-            with open('Syntax-Violations', 'w') as filehand:
-                for line in exception_list:
-                    filehand.write(line + os.linesep)
-        # else:
-        ## Checking if the CHANGELOG.md exists. If so, then temporarily rename it
-        ## write the entry for each of the feat, fix and chore, and then append earlier
-        ## CHANGELOG text now in a temp file into it and clean up. Else, write completly 
-        ## new Changelog entry
-        earlier_changelog_path = pathlib.Path('CHANGELOG.md')
-        if earlier_changelog_path.exists():
-            os.rename('CHANGELOG.md', 'TEMP.md')
-        changelog_filehand = open('CHANGELOG.md', 'w')
-        version_number = "1.0.0"
-        current_datetime = datetime.datetime.now()
-        ## Writing current version number and date of the version creation
-        changelog_filehand.write('# {} ({})'.format(version_number, current_datetime.strftime('%Y-%m-%d')) + os.linesep)
-        
-        if len(fix_list) > 0:
-            changelog_filehand.write('### Bug Fixes' + os.linesep)
-            for line in fix_list:
-                changelog_filehand.write(line + os.linesep)
-
-        if len(feat_list) > 0:
-            changelog_filehand.write('### Features' + os.linesep)
-            for line in feat_list:
-                changelog_filehand.write(line + os.linesep)
-
-        if len(chore_list) > 0:
-            changelog_filehand.write('### Improvements' + os.linesep)
-            for line in chore_list:
-                changelog_filehand.write(line + os.linesep)
-
-        if earlier_changelog_path.exists():
-            with open('TEMP.md') as filehand:
-                for line in filehand:
-                    changelog_filehand.write(line + os.linesep)
-            os.remove('TEMP.md')
-       
-        changelog_filehand.close()
-
     f.close()
 
+    ## Checking to see if there are any syntax violating commits for the current changelog
+    ## if so, then writing it to a reference file with commit hash and subject for the 
+    ## relase manage to git rebase and squah them. Else, writing the changelog.
+    if len(exception_list) != 0:
+        with open('Syntax-Violations', 'w') as filehand:
+            for line in exception_list:
+                filehand.write(line + os.linesep)
+    # else:
+    ## Checking if the CHANGELOG.md exists. If so, then temporarily rename it
+    ## write the entry for each of the feat, fix and chore, and then append earlier
+    ## CHANGELOG text now in a temp file into it and clean up. Else, write completly 
+    ## new Changelog entry
+    if os.path.isfile('CHANGELOG.md'):
+        os.rename('CHANGELOG.md', 'TEMP.md')
+    changelog_filehand = open('CHANGELOG.md', 'w')
+    version_number = "1.0.0"
+    current_datetime = datetime.datetime.now()
+    ## Writing current version number and date of the version creation
+    changelog_filehand.write('# {} ({})'.format(version_number, current_datetime.strftime('%Y-%m-%d')) + os.linesep)
+    
+    if len(fix_list) > 0:
+        changelog_filehand.write('### Bug Fixes' + os.linesep)
+        for line in fix_list:
+            changelog_filehand.write(line + os.linesep)
+
+    if len(feat_list) > 0:
+        changelog_filehand.write('### Features' + os.linesep)
+        for line in feat_list:
+            changelog_filehand.write(line + os.linesep)
+
+    if len(chore_list) > 0:
+        changelog_filehand.write('### Improvements' + os.linesep)
+        for line in chore_list:
+            changelog_filehand.write(line + os.linesep)
+
+    if os.path.isfile('TEMP.md'):
+        with open('TEMP.md') as filehand:
+            for line in filehand:
+                changelog_filehand.write(line)
+        os.remove('TEMP.md')
+    
+    changelog_filehand.close()
 
 if __name__ == '__main__':
     if len(sys.argv)==2:
